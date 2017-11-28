dbfeec54aeefcbb41b7cd7acae864fd298cd7aa9
sohan yadav <32355277+yadavsohan@users.noreply.github.com>
2017-11-29 02:02:57 +0530

    I added several fields in books class and i also created two new classes  which is Student and Department;
    @Yadav Sohan and @Tyagi Suryakant

--- core/src/main/java/hu/uni/miskolc/iit/sweng/libraryManagement/core/model/Book.java
+++ core/src/main/java/hu/uni/miskolc/iit/sweng/libraryManagement/core/model/Book.java
@@ -1,25 +1,50 @@
 package hu.uni.miskolc.iit.sweng.libraryManagement.core.model;
 
+import com.sun.xml.internal.ws.spi.db.DatabindingException;
 import hu.uni.miskolc.iit.sweng.libraryManagement.model.Author;
 
+import java.util.Date;
 import java.util.List;
 
 public class Book {
 
-    private List<Author> author;
-    private final String title;
+    private List<Author> _author;
+    private final String _title;
+    private int _Department_Id;
+   private String _Name;
+    private String _Edition;
+    private Double __Price;
 
-    public Book(List<Author> author, String title) {
-        this.author = author;
-        this.title = title;
+    public Book(List<Author> author, String title,int Department_Id, String Name,String Edition,Double Price) {
+        this._author=author;
+        this._title = title;
+        this._Department_Id=Department_Id;
+        this._Name=Name;
+        this._Edition=Edition;
+        this.__Price=Price;
     }
 
-    public List<Author> getAuthor() {
-        return author;
+    public int get_Department_Id() {
+        return _Department_Id;
     }
 
-    public String getTitle() {
-        return title;
+    public Double get__Price() {
+        return __Price;
     }
 
+    public List<Author> get_author() {
+        return _author;
+    }
+
+    public String get_Edition() {
+        return _Edition;
+    }
+
+    public String get_Name() {
+        return _Name;
+    }
+
+    public String get_title() {
+        return _title;
+    }
 }
